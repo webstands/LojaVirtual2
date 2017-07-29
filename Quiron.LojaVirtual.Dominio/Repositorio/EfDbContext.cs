a356245898a20c1ad5da2ef0ea1976ad57380871
webstands <webstands@gmail.com>
2017-07-29 14:18:36 -0300

    Até a Sexta aula do curso

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using Quiron.LojaVirtual.Dominio.Entidades;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class EfDbContext : DbContext    
+    {
+        /*DbSet Representa uma coleção de entidades no contexto.
+          Serve para mapear a classe*/
+          public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuider)
+        {
+            modelBuider.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuider.Entity<Produto>().ToTable("Produtos");
+        }
+    }
+}
